d92d3a9252f57499460cf0371806f3c7066fff52
ASHGT <ashgeneraltraders@gmail.com>
2024-10-12 07:14:55 -0700

    adding the htaccess

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,10 @@
 module.exports = {
     apps: [
       {
-        name: 'nextjs-app',     // Name of the application
-        script: 'npm',          // Using npm to run the Next.js scripts
-        args: 'start',          // Runs 'npm start' to start the Next.js server
-        instances: 'max',       // Auto-scale to all available CPUs
-        exec_mode: 'cluster',   // Enable load balancing between instances
-        watch: false,           // Disable file watching for production
+        name: 'nextjs-app',
+        script: 'npm',
+        args: 'start',
         env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
           NODE_ENV: 'production',
         },
       },
