cf5172c95f1082b840563b4cd5dc84296975333f
ASHGT <ashgeneraltraders@gmail.com>
2024-10-12 07:29:55 -0700

    adding the htaccess

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     apps: [
       {
-        name: 'CashCoin',
+        name: 'nextjs-app',
         script: 'npm',
         args: 'start',
+        instances: 'max',   // or a specific number of instances, e.g., 4
+        exec_mode: 'cluster',  // Use 'cluster' mode instead of 'fork'
         env: {
           NODE_ENV: 'production',
         },
