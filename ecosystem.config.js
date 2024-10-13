2ebd43e30c96fe6af11e4455acda4204a32ee7ef
ASHGT <ashgeneraltraders@gmail.com>
2024-10-13 04:03:49 -0700

    adding one instance at a time

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'CashCoins',
         script: 'npm',
         args: 'start',
-        instances: 'max',   // or a specific number of instances, e.g., 4
+        instances: '1',   // or a specific number of instances, e.g., 4
         exec_mode: 'cluster',  // Use 'cluster' mode instead of 'fork'
         env: {
           NODE_ENV: 'production',
